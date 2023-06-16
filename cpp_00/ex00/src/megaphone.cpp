9ee5be9f2f3bbc67badab7e1b60bf42b62cf1463
Martijn Weverling <mweverli@student.codam.nl>
2023-06-16 15:16:31 +0200

    cpp_00 : ex00: finished ;

--- cpp_00/ex00/src/megaphone.cpp
+++ cpp_00/ex00/src/megaphone.cpp
@@ -1,4 +1,23 @@
-int main()
+#include <iostream>
+
+std::string str_to_upper(std::string str) {
+	int		strlen = str.length();
+
+	for (int i = 0; i < strlen; i++) {
+		if (islower(str[i]))
+			str[i] = toupper(str[i]);
+	}
+
+	return (str);
+}
+
+int main(int argc, char **argv)
 {
-	return (1);
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++) {
+		std::cout << str_to_upper(argv[i]) << " ";
+	}
+	std::cout << std::endl;
+	return (0);
 }
