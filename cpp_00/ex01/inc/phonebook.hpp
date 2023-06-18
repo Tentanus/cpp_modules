654fb4097ff2c8c845ab60b39d443bc4be9af84a
Tentanus <tentanusw@gmail.com>
2023-06-18 21:24:30 +0200

    cpp_00: ex01: i'm confuse, i'm overloading a function. idk what is happen ;

--- cpp_00/ex01/inc/phonebook.hpp
+++ cpp_00/ex01/inc/phonebook.hpp
@@ -1,7 +1,13 @@
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 #include <contact.hpp>
- 
+
+typedef enum e_err_num {
+	SUCCESS = 1,
+	ERR_INP,
+	ERR_COUNT
+}	t_err_num;
+
 class PhoneBook {
 private:
 	Contact		list[9];
--- cpp_00/ex01/src/main.cpp
+++ cpp_00/ex01/src/main.cpp
@@ -3,23 +3,42 @@
 
 #include <phonebook.hpp>
 
+static const std::string g_err_msg[ERR_COUNT] = {
+		"SUCCES",
+		"incorrect input: use ADD, SEARCH, EXIT",
+		"ERR_COUNT"
+};
+
+int	phonebook_error(int err_code) {
+	std::cerr << "phonebook: " << g_err_msg[err_code] << std::endl;
+	return (err_code);
+}
+
+void	phonebook_add(void) {
+	std::cout << "inside phonebook_add" << std::endl;
+}
+
+
+void	phonebook_search(void) {
+	std::cout << "inside phonebook_search" << std::endl;
+}
+
 int main() {
-	std::string			input;
-	std::istringstream	iss;
-	PhoneBook			phonebook;
+	std::string	input;
+	PhoneBook	phonebook;
 
 	while (1) {
+		std::cout << "Enter command: ";
 		std::getline(std::cin, input);
-		input.
-		switch (word) {
-			case 'EXIT':
-				return (0);
-			case 'ADD':
-				;
-			case 'SEARCH':
-				;
-			default:
-				cerr << "FAULTY INPUT, TRY AGAIN" << std::endl;
-		}
+		input.erase(0, input.find_first_not_of(" \t\v\r\f"));
+		input.erase(input.find_last_not_of(" \t\v\r\f"), input.end());
+		if (input == "EXIT")
+			return (0);
+		else if (input == "ADD")
+			phonebook_add();
+		else if (input == "SEARCH")
+			phonebook_search();
+		else
+			return (phonebook_error(ERR_INP));
 	}
 }
