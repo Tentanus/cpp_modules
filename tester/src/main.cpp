449710d8a3fcbf839205a942b55856ad636e5acb
Tentanus <tentanusw@gmail.com>
2023-08-08 01:35:17 +0200

    cpp_test: printing floating point bits

--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -3,29 +3,57 @@
 #include <string>
 #include <cstdlib>
 
+void	print_float_bit_reversed(float f0)
+{
+	int	i0;
+
+	i0 = * (int *) &f0;
+
+	for (size_t i = 0; i < sizeof(float) * 8; i++)
+	{
+		if (i == 23 || i == 31 || i == 32)
+			std::cout << " ";
+		if (((i0 >> i) & 0x1))
+			std::cout << "1";
+		else
+			std::cout << "0";
+	}
+	std::cout << "\t" << f0 << std::endl;
+}
+
+void	print_float_bit(float f0)
+{
+	int	i0;
+
+	i0 = * (int *) &f0;
+
+	for (size_t i = 0; i < sizeof(float) * 8; i++)
+	{
+		if (i == 1 || i == 9)
+			std::cout << " ";
+		if (((i0 << i) & 0x80000000) >> 31)
+			std::cout << "1";
+		else
+			std::cout << "0";
+	}
+	std::cout << "\t" << f0 << std::endl;
+}
+
 int main()
 {
-	std::string				inp;
-//	std::string::size_type	sz;
-//	int						val_stoi;
-	int						val_atoi;
 
+	std::cout << "sizeof float: " << sizeof(float) << "bytes" << std::endl; // 4 bytes
 
-	std::cout << std::endl << "Give detailed overview of Contact [1 - 8] or EXIT" << std::endl;
-	std::getline(std::cin, inp);
-
-	std::cout << "INPUT: " << inp <<  std::endl;
-
-	if (inp == "EXIT")
-		return (0);
-
-	val_atoi = std::atoi(inp.c_str());
-	std::cout << "val atoi:\t" << val_atoi << std::endl;
-
-//	val_stoi = std::stoi(inp, &sz);
-//	std::cout << "val:\t" << val_stoi << std::endl << "sz:\t" << sz << std::endl;
-
-	std::cout << "we are finished." << std::endl;
+	std::cout << "S EEEEEEEE MMMMMMMMMMMMMMMMMMMMMMM" << std::endl;
+	std::cout << "\n";
+	print_float_bit(0);
+	print_float_bit(1);
+	std::cout << "\n";
+	print_float_bit(0.15625);
+	print_float_bit(0.1);
+	print_float_bit(0.2);
+	print_float_bit(0.3);
+	print_float_bit(0.4);
+	print_float_bit(0.5);
 	return (0);
-
 }
