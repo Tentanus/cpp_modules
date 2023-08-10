afde04f441dc570ebfc5a91690c169d02758a6bd
Martijn Weverling <mweverli@student.codam.nl>
2023-08-10 18:43:02 +0200

    cpp_01: ex06: finished

--- cpp_01/ex04/src/main.cpp
+++ cpp_01/ex04/src/main.cpp
@@ -1,68 +1,69 @@
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-typedef enum e_errcode {
+typedef enum e_errcode
+{
 	ERR_ARG,
 	ERR_INP,
 	ERR_INF,
 	ERR_OUTF,
 	ERR_
-} t_errcode ;
+} t_errcode;
 
-int	rep_error(size_t err)
+int rep_error(size_t err)
 {
 	std::string err_msg[] = {
-		"give 4 arguments", //ARG
-		"give non empty string input", //INP
-		"unable to open file", //INF
-		"unable to create output file" //OUTF
+		"give 4 arguments",			   // ARG
+		"give non empty string input", // INP
+		"unable to open file",		   // INF
+		"unable to create output file" // OUTF
 	};
 
 	std::cerr << "repline: " << err_msg[err] << std::endl;
 	return (err);
 }
 
-void	replace_line(std::string &line, std::string s1, std::string s2)
+void replace_line(std::string &line, std::string s1, std::string s2)
 {
-	size_t			pos = line.find(s1, 0);
-	const size_t	s2_len = s2.length();
-
+	size_t pos = line.find(s1, 0);
+	const size_t s2_len = s2.length();
 
 	while (pos != std::string::npos)
 	{
-		line.replace(pos, s1.length(), s2);
+		line.erase(pos, s1.length());
+		line.insert(pos, s2);
 		pos += s2_len;
 		pos = line.find(s1, pos);
 	}
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc != 4)
 		return (rep_error(ERR_ARG));
 
-	const std::string	if_name = argv[1];
-	std::ifstream		infile(if_name);
+	const std::string if_name = argv[1];
+	std::ifstream infile(if_name);
 	if (!infile.is_open())
 		return (rep_error(ERR_INF));
 
-	const std::string	of_name = if_name + ".replace";
-	std::ofstream		outfile(of_name);
+	const std::string of_name = if_name + ".replace";
+	std::ofstream outfile(of_name);
 	if (!outfile.is_open())
 		return (rep_error(ERR_OUTF));
 
-	const std::string	s1 = argv[2];
-	const std::string	s2 = argv[3];
+	const std::string s1 = argv[2];
+	const std::string s2 = argv[3];
 	if (s1.empty())
 		return (rep_error(ERR_INP));
 
-	std::string			line;
+	std::string line;
 	while (infile)
 	{
 		std::getline(infile, line);
 		if (infile.eof() && line.empty())
-			break ;
+			break;
 		replace_line(line, s1, s2);
 		outfile << line << std::endl;
 		line.clear();
--- cpp_01/ex06/src/Harl.cpp
+++ cpp_01/ex06/src/Harl.cpp
@@ -1,37 +1,46 @@
 
 #include <Harl.hpp>
 
-size_t	Harl::complain (std::string lvl)
+size_t Harl::complain(std::string lvl)
 {
 	const size_t max_complaints = sizeof(func_ptr_list) / sizeof(func_ptr);
-	size_t		 complain_lvl;
+	size_t complain_lvl = 0;
+	size_t i;
 
-	for (size_t i = 0; i < max_complaints; i++) {
-		if (lvl == lvl_list[i]) {
-			complain_lvl = i;
+	for (i = 0; i < max_complaints; i++)
+	{
+		complain_lvl = i;
+		if (!lvl.compare(lvl_list[i]) && lvl.length() == lvl_list[i].length())
 			break;
-		}
 	}
 
+	if (lvl.compare(lvl_list[max_complaints - 1]) &&
+		lvl.length() != lvl_list[max_complaints - 1].length() &&
+		i == complain_lvl)
+		complain_lvl++;
 	return (complain_lvl);
 }
 
-void	Harl::debug (void) {
+void Harl::debug(void)
+{
 	std::cout << "f:" << __FILE__ << " l:" << __LINE__ << "\t "
 			  << "DEBUG INFO" << std::endl;
 }
 
-void	Harl::error (void) {
+void Harl::error(void)
+{
 	std::cout << "f:" << __FILE__ << " l:" << __LINE__ << "\t "
 			  << "ERROR INFO" << std::endl;
 }
 
-void	Harl::info (void) {
+void Harl::info(void)
+{
 	std::cout << "f:" << __FILE__ << " l:" << __LINE__ << "\t "
 			  << "INFO INFO" << std::endl;
 }
 
-void	Harl::warning (void) {
+void Harl::warning(void)
+{
 	std::cout << "f:" << __FILE__ << " l:" << __LINE__ << "\t "
 			  << "WARNING INFO" << std::endl;
 }
