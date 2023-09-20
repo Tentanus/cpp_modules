55e160263b61cc5535ae046e3b5e552b140bc987
Martijn Weverling <mweverli@student.codam.nl>
2023-09-20 17:37:12 +0200

    cpp_03: exo2: copied changes from ex00 & ex01 into ex02

--- cpp_03/ex02/src/main.cpp
+++ cpp_03/ex02/src/main.cpp
@@ -1,54 +1,36 @@
 
-#include "FragTrap.hpp"
 #include <iostream>
 
 #include <ClapTrap.hpp>
 #include <ScavTrap.hpp>
 
-#define BORDERMESSAGE                                                          \
+#define MSG_BORDER                                                             \
 	"=----------------------------------------------------------="
+
 int main()
 {
 	ClapTrap CT_1("Henk");
-	ScavTrap ST_1("Jan");
-	FragTrap FT_1("Piet");
+	ScavTrap ST_1("Jaap");
 
-	CT_1.attack("Jan");
-	CT_1.attack("Piet");
-	ST_1.takeDamage(CT_1.get_attack());
-	FT_1.takeDamage(CT_1.get_attack());
 	std::cout << "\n";
-
-	std::cout << BORDERMESSAGE << std::endl;
-	CT_1.status();
-	ST_1.status();
-	FT_1.status();
-	std::cout << BORDERMESSAGE << "\n" << std::endl;
-
-	ST_1.beRepaired(2);
-	FT_1.beRepaired(2);
-	std::cout << "\n";
-
-	std::cout << BORDERMESSAGE << std::endl;
-	CT_1.status();
-	ST_1.status();
-	FT_1.status();
-	std::cout << BORDERMESSAGE << "\n" << std::endl;
 
 	ST_1.attack("Henk");
 	CT_1.takeDamage(ST_1.get_attack());
-	FT_1.attack("Jan");
-	ST_1.takeDamage(FT_1.get_attack());
-	std::cout << "\n";
 
-	std::cout << BORDERMESSAGE << std::endl;
+	std::cout << "\n" << MSG_BORDER << std::endl;
 	CT_1.status();
-	ST_1.status();
-	FT_1.status();
-	std::cout << BORDERMESSAGE << "\n" << std::endl;
-
-	ST_1.guardGate();
 	std::cout << "\n";
-	FT_1.highFivesGuys();
+	ST_1.status();
+	std::cout << MSG_BORDER << "\n" << std::endl;
+
+	ST_1.beRepaired(2);
+	CT_1.beRepaired(2);
+
+	std::cout << "\n" << MSG_BORDER << std::endl;
+	CT_1.status();
+	std::cout << "\n";
+	ST_1.status();
+	std::cout << MSG_BORDER << "\n" << std::endl;
+
 	return (0);
 }
