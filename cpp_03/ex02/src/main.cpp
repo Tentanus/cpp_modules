ccec05d9cd594db5cc6c09e077aab25bb98b4e71
Martijn Weverling <mweverli@student.codam.nl>
2023-09-20 17:45:36 +0200

    cpp_03: ex02: update to main.cpp

--- cpp_03/ex02/src/main.cpp
+++ cpp_03/ex02/src/main.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 
-#include <ClapTrap.hpp>
+#include <FragTrap.hpp>
 #include <ScavTrap.hpp>
 
 #define MSG_BORDER                                                             \
@@ -9,28 +9,40 @@
 
 int main()
 {
-	ClapTrap CT_1("Henk");
+	FragTrap FT_1("Henk");
 	ScavTrap ST_1("Jaap");
 
 	std::cout << "\n";
 
-	ST_1.attack("Henk");
-	CT_1.takeDamage(ST_1.get_attack());
+	FT_1.attack("Jaap");
+	ST_1.takeDamage(FT_1.get_attack());
 
 	std::cout << "\n" << MSG_BORDER << std::endl;
-	CT_1.status();
+	FT_1.status();
 	std::cout << "\n";
 	ST_1.status();
 	std::cout << MSG_BORDER << "\n" << std::endl;
 
-	ST_1.beRepaired(2);
-	CT_1.beRepaired(2);
+	FT_1.attack("Jaap");
+	ST_1.takeDamage(FT_1.get_attack());
+	FT_1.attack("Jaap");
+	ST_1.takeDamage(FT_1.get_attack());
+	FT_1.attack("Jaap");
+	ST_1.takeDamage(FT_1.get_attack());
 
 	std::cout << "\n" << MSG_BORDER << std::endl;
-	CT_1.status();
+	FT_1.status();
 	std::cout << "\n";
 	ST_1.status();
 	std::cout << MSG_BORDER << "\n" << std::endl;
+
+	ST_1.guardGate();
+	FT_1.highFivesGuys();
+
+	std::cout << "\n";
+
+	//	FT_1.guardGate();
+	//	ST_1.highFivesGuys();
 
 	return (0);
 }
