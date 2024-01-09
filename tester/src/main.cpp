dc968d6ec70137811a6767007be3cbd83ce42637
Martijn Weverling <mweverli@student.codam.nl>
2024-01-09 17:06:52 +0100

    working with stat()

--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -1,12 +1,17 @@
 
-#include <algorithm>
+#include <cstdint>
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
 #include <string>
-#include <vector>
 
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <dirent.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 
 #include <tester.hpp>
 
@@ -16,14 +21,33 @@ int main(int argc, char *argv[])
 	(void)argv;
 
 	std::string str1 = "src";
-	std::string str2 = "./src/main.cpp";
 	bool val = std::filesystem::is_directory(str1);
 	std::cout << "does " << str1 << " exist: " << (val ? "TRUE" : "FALSE")
 			  << std::endl;
-	val = std::filesystem::exists(str2);
-	std::cout << "does " << str2 << " exist: " << (val ? "TRUE" : "FALSE")
-			  << std::endl;
+
+	std::string path = "./src";
+	DIR *dir = opendir(path.c_str());
+	struct dirent *dp;
+	std::filesystem::path pwd = std::filesystem::current_path();
+
+	while ((dp = readdir(dir)) != NULL)
+	{
+		std::string path = pwd.string();
+		std::string filepath = path + "/src/" + dp->d_name;
+
+		struct stat filestat;
+		if (stat(filepath.c_str(), &filestat) == -1)
+		{
+			std::cout << "NO STAT\t" << filepath << "\t"
+					  << std::string(strerror(errno)) << "\n";
+			continue;
+		}
+		std::cout << std::to_string((intmax_t)filestat.st_size) << "\t"
+				  << sizeof(filestat.st_size) << "\t" << filepath << std::endl;
+	}
 }
+
+// I don't get why i can't use stat to find the size wtf.
 
 // You can compare Vector::iterators originating from the same
 // vector
