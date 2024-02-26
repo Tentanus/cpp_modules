a730a9ab55580ce8dbf8c6c03b18e3d7df4fa76a
Martijn Weverling <mweverli@student.codam.nl>
2024-02-26 12:52:41 +0100

    Added OCF to ex01

--- cpp_06/ex01/src/Serializer.cpp
+++ cpp_06/ex01/src/Serializer.cpp
@@ -9,9 +9,9 @@ std::ostream &operator<<(std::ostream &os, const Data &ptr)
 					"y:\t" << ptr.y << "\n" << std::endl;
 	return (os);
 }
-// clang-format off
+// clang-format on
 
-uintptr_t Serializer::serialize(Data* ptr)
+uintptr_t Serializer::serialize(Data *ptr)
 {
 	return reinterpret_cast<uintptr_t>(ptr);
 }
@@ -19,4 +19,37 @@ uintptr_t Serializer::serialize(Data* ptr)
 Data *Serializer::deserialize(uintptr_t raw)
 {
 	return reinterpret_cast<Data *>(raw);
+}
+
+//-------------------Orthodox Canonical Form-------------------//
+
+Serializer::Serializer()
+{
+#ifdef MSG
+	std::cout << "Called\tSerializer\t\tConstructor:\tDefualt" << std::endl;
+#endif
+}
+
+Serializer::Serializer(const Serializer &rhs)
+{
+#ifdef MSG
+	std::cout << "Called\tSerializer\tConstructor:\tCopy" << std::endl;
+#endif
+	(void)rhs;
+}
+
+Serializer &Serializer::operator=(const Serializer &rhs)
+{
+#ifdef MSG
+	std::cout << "Called\tSerializer\tCopy Assignment Operator" << std::endl;
+#endif
+	(void)rhs;
+	return (*this);
+}
+
+Serializer::~Serializer()
+{
+#ifdef MSG
+	std::cout << "Called\tSerializer\t\tDeconstructor" << std::endl;
+#endif
 }
