3f167991a806d468a25ecee4aa5cb81e2eb5d212
Martijn Weverling <mweverli@student.codam.nl>
2024-05-21 17:36:35 +0200

    cpp_05: readability update

--- cpp_05/ex01/src/main.cpp
+++ cpp_05/ex01/src/main.cpp
@@ -5,12 +5,13 @@
 
 #define MSG_BORDER "-----------------------------------------------"
 
-bool t_constructors(Bureaucrat **B1, Bureaucrat **B2, Form **F1)
+bool t_constructors(Bureaucrat **B1, Bureaucrat **B2, Bureaucrat**B3, Form **F1)
 {
 	try
 	{
 		*B1 = new Bureaucrat("Jimmy", 40);
-		*B2 = new Bureaucrat("James", 80);
+		*B2 = new Bureaucrat("Johny", 60);
+		*B3 = new Bureaucrat("James", 80);
 		*F1 = new Form("Garbage License", 75, 50);
 	}
 	catch (std::exception &e)
@@ -22,13 +23,17 @@ bool t_constructors(Bureaucrat **B1, Bureaucrat **B2, Form **F1)
 	return (true);
 }
 
-bool t_signing(Bureaucrat &B1, Bureaucrat &B2, Form &F1)
+bool t_signing(Bureaucrat &B1, Bureaucrat &B2, Bureaucrat &B3, Form &F1)
 {
+	B3.signForm(F1);
+	std::cout << "\n" << F1 << std::endl;
+	std::cout << "\n" << MSG_BORDER << "\n" << std::endl;
 	B2.signForm(F1);
-
+	std::cout << "\n" << F1 << std::endl;
+	std::cout << "\n" << MSG_BORDER << "\n" << std::endl;
 	B1.signForm(F1);
-	B1.signForm(F1);
-
+	std::cout << "\n" << F1 << std::endl;
+	std::cout << "\n" << MSG_BORDER << "\n" << std::endl;
 	return (true);
 }
 
@@ -36,21 +41,21 @@ int main()
 {
 	Bureaucrat *B1;
 	Bureaucrat *B2;
+	Bureaucrat *B3;
 	Form *F1;
 
-	if (!t_constructors(&B1, &B2, &F1))
+	if (!t_constructors(&B1, &B2, &B3, &F1))
 		return (1);
 
 	std::cout << MSG_BORDER << std::endl;
 	std::cout << "\nOUTPUT: \n";
-
 	std::cout << *B1 << std::endl;
 	std::cout << *B2 << std::endl;
+	std::cout << *B3 << std::endl;
 	std::cout << *F1 << std::endl;
+	std::cout << "\n" << MSG_BORDER << "\n" << std::endl;
 
-	std::cout << "\n" << MSG_BORDER << std::endl;
-
-	if (!t_signing(*B1, *B2, *F1))
+	if (!t_signing(*B1, *B2, *B3, *F1))
 		return (1);
 
 	return (0);
