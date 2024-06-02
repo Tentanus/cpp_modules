5078ffc7c7d200506b3a863bfe0418c63e908093
tentanus <tentanusw@gmail.com>
2024-06-02 21:42:41 +0200

    cpp08: ex00 finished

---
+++ cpp_08/ex00/src/main.cpp
@@ -0,0 +1,30 @@
+#include "easyfind.hpp"
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main() {
+    try {
+        std::vector<int> vec;
+		for (int i = 1 ; i < 5 ; i++)
+			vec.push_back(i);
+
+        std::vector<int>::iterator it = easyfind(vec, 3);
+        std::cout << "Found value: " << *it << std::endl;
+
+        std::list<int> lst;
+		for (int i = 1 ; i < 5 ; i++)
+			lst.push_back(i);
+
+        std::list<int>::iterator it2 = easyfind(lst, 3);
+        std::cout << "Found value: " << *it2 << std::endl;
+
+        std::list<int>::iterator it3 = easyfind(lst, 60);
+        std::cout << "Found value: " << *it3 << std::endl;
+    } catch (const NotFoundException& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    return 0;
+}
