86abb3a1025b6d3dfbca81c5e72aa69d899b6111
Martijn Weverling <mweverli@student.codam.nl>
2024-06-04 19:21:01 +0200

    cpp08: ex01: finished

--- cpp_08/ex01/src/Span.cpp
+++ cpp_08/ex01/src/Span.cpp
@@ -4,77 +4,88 @@ void Span::addNumber(int value)
 {
 	if (_size == _index)
 		throw SpanExceedSizeException(_size);
+#ifdef MSG
+	std::cout << "addNumber(" << std::to_string(value) << ")" << std::endl;
+#endif
 	_set.emplace(value);
 	_index++;
 }
 
 void Span::addRange(int start, int end, int step)
 {
-	for (int i = start ; i < end ; i += step)
+	for (int i = start; i < end; i += step)
 	{
-		std::cout << "addNumber(" << std::to_string(i) << ")" << std::endl;
-		try { addNumber(i);}
-    	catch (Span::NoSpanException &e) {break;}
+		try
+		{
+			addNumber(i);
+		}
+		catch (Span::NoSpanException &e)
+		{
+			break;
+		}
 	}
 }
 
+// constant
 int Span::longestSpan()
 {
 	if (_set.size() < 2)
 		throw NoSpanException(_index);
 	std::set<int>::iterator big = _set.begin();
 	std::set<int>::iterator end = --_set.end();
-	// std::cout << *big << "\t" << *end << std::endl;
 	return (*end - *big);
 }
 
+// O(N)
 int Span::shortestSpan()
 {
 	if (_set.size() < 2)
 		throw NoSpanException(_index);
 	int ret = INT_MAX;
-	for (std::set<int>::iterator it = ++_set.begin() ;
-		 it != _set.end() && ret != 1 ; it++) {
-		/*
-		std::cout << "\nround:\t" << std::to_string(*it) << std::endl;
-		std::cout << "ret:\t" << std::to_string(ret) << std::endl;
-		*/
+	for (std::set<int>::iterator it = ++_set.begin();
+		 it != _set.end() && ret != 1; it++)
+	{
 
-		std::set<int>::iterator diff; 
+		std::set<int>::iterator diff;
 		int tmp;
 		diff = --it;
 		it++;
 		tmp = *it - *diff;
-		// std::cout << "--:\t" << std::to_string(*it) << "\t" << std::to_string(*diff) << std::endl;
+#ifdef MSG
+		std::cout << "\ndiff:\t" << std::to_string(*diff) << " "
+				  << std::to_string(*it) << "\t| " << std::to_string(tmp);
+#endif
 		if (tmp < ret)
 			ret = tmp;
 	}
+#ifdef MSG
+	std::cout << std::endl;
+#endif
 	return (ret);
 }
 
 //-------------------Orthodox Canonical Form-------------------//
 
-Span::Span(int size)
-: _size(size), _index(0), _set()
+Span::Span(int size) : _size(size), _index(0), _set()
 {
-	#ifdef MSG
-		std::cout << "Called\tSpan\tConstructor:\tSized" << std::endl;
-	#endif
+#ifdef MSG
+	std::cout << "Called\tSpan\tConstructor:\tSized" << std::endl;
+#endif
 }
 
 Span::Span(const Span &rhs)
-: _size(rhs._size), _index(rhs._index), _set(rhs._set)
+	: _size(rhs._size), _index(rhs._index), _set(rhs._set)
 {
-	#ifdef MSG
-		std::cout << "Called\tSpan\tConstructor:\tCopy" << std::endl;
-	#endif
+#ifdef MSG
+	std::cout << "Called\tSpan\tConstructor:\tCopy" << std::endl;
+#endif
 }
 
 Span &Span::operator=(const Span &rhs)
 {
-	#ifdef MSG
-		std::cout << "Called\tSpan\tCopy Assignment Operator" << std::endl;
-	#endif
+#ifdef MSG
+	std::cout << "Called\tSpan\tCopy Assignment Operator" << std::endl;
+#endif
 	if (this == &rhs)
 		return (*this);
 	_size = rhs._size;
@@ -85,7 +96,7 @@ Span &Span::operator=(const Span &rhs)
 
 Span::~Span()
 {
-	#ifdef MSG
-		std::cout << "Called\tSpan\tDeconstructor" << std::endl;
-	#endif
+#ifdef MSG
+	std::cout << "Called\tSpan\tDeconstructor" << std::endl;
+#endif
 }
--- cpp_08/ex01/src/main.cpp
+++ cpp_08/ex01/src/main.cpp
@@ -3,20 +3,33 @@
 
 int main(void)
 {
-    Span spn(4);
-    spn.addRange(3, 6, 2);
-    try {spn.addNumber(11);}
-    catch (Span::SpanExceedSizeException &e) {}
+	Span spn(4);
+	spn.addRange(3, 8, 2);
+	try
+	{
+		spn.addNumber(11);
+	}
+	catch (Span::SpanExceedSizeException &e)
+	{
+	}
 
-    try{
-        std::cout << "longest:\t" << std::to_string(spn.longestSpan()) << std::endl;
-    }
-    catch (Span::NoSpanException &e) {}
+	try
+	{
+		std::cout << "longest:\t" << std::to_string(spn.longestSpan())
+				  << std::endl;
+	}
+	catch (Span::NoSpanException &e)
+	{
+	}
 
-    try{
-        std::cout << "shortest:\t" << std::to_string(spn.shortestSpan()) << std::endl;
-    }
-    catch (Span::NoSpanException &e) {}
+	try
+	{
+		std::cout << "shortest:\t" << std::to_string(spn.shortestSpan())
+				  << std::endl;
+	}
+	catch (Span::NoSpanException &e)
+	{
+	}
 
-    return (0);
+	return (0);
 }
