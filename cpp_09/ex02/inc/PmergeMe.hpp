d8a0fb9fac5fe0895789243af4413c926a479835
Martijn Weverling <mweverli@student.codam.nl>
2024-06-24 20:32:05 +0200

    cpp09: ex02: start PMergeMe

---
+++ cpp_09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,5 @@
+#ifndef PMERGEME_HPP
+
+#include <iostream>
+
+#endif // PMERGEME_HPP
---
+++ cpp_09/ex02/src/main.cpp
@@ -0,0 +1,11 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <input file>" << std::endl;
+		return 1;
+	}
+	return 0;
+}
